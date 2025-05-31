09a8f5ae16c9a43b2d54f688a14721206051a4d8
venu <venu.kalaimagal@gmail.com>
2025-05-31 13:54:08 +0530

    first

---
+++ vite-project/src/Auth/Login.jsx
@@ -0,0 +1,38 @@
+ import React from 'react'
+import { useState } from 'react'
+import axios from 'axios'
+const Login = () => {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [message, setMessage] = useState('');
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            const res = await axios.post('https://kongu2025.onrender.com/user/login', { username, password })
+            setMessage(res.data.message)
+        } catch (err) {
+            setMessage('Login failed')
+            console.log(err);
+           
+        }
+    }
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input type="text"
+                    value={username}
+                    placeholder='enter the username'
+                    onChange={(e) => setUsername(e.target.value)} />
+                <input type="password"
+                    value={password}
+                    placeholder='enter the password'
+                    onChange={(e) => setPassword(e.target.value)} />
+                <button type='submit'>Login</button>
+                <p>{message}</p>
+            </form>
+        </div>
+    )
+}
+
+
+export default Login
